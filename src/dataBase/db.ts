e304b4aed34eb1b3241949c34009c65a9cab831e
emillyalmeida <emillyalmeidan@gmail.com>
2022-07-01 09:52:57 -0300

    feat: creta data base and model URL

---
+++ src/dataBase/db.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+import { Config } from "../config/constrants";
+
+export class DBconection {
+  static async Conection() {
+    try {
+      await mongoose.connect(Config.MONGO_CONNECTION);
+      console.log("db connected");
+    } catch (error) {}
+  }
+}
