ae31b6d8bcfdc59ca6984b2d3df4beabccbe8230
emillyalmeida <emillyalmeidan@gmail.com>
2022-07-01 09:56:48 -0300

    feat: add typegoose and connect db

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import express from "express";
 import { UrlController } from "./controller/urlController";
+import { DBconection } from "./dataBase/db";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+DBconection.Conection();
+
 app.post("/shortner", UrlController.shortUrl);
+app.get("/:hash", UrlController.Redirect);
 
 app.listen(3333, () => {
   console.log("app executando na porta 3333");
